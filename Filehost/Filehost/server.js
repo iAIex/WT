d0aa56474474afdd8ab57e460aec40523f825085
iAIex <32507826+iAIex@users.noreply.github.com>
2017-11-20 15:33:04 +0100

    Added VS Node Project

---
+++ Filehost/Filehost/server.js
@@ -0,0 +1,27 @@
+'use strict';
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//Listening on Port 1337
+http.listen(1337, function () {
+    console.log('Server up on 1337')
+})
+
+// ---- ROUTING ----
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/websites/index.html')
+})
+
+app.get('/impressum.html', function (req, res) {
+    res.sendFile(__dirname + '/websites/impressum.html')
+})
+
+
+io.on('connection', function (socket) {
+    console.log('Connection recieved')
+    io.emit('hallo',"Ich bin peter")
+});
+
+io.on('chat-in', function (socket) { console.log(socket) });
