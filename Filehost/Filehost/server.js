c725a2612309d2b630807d8655c5e93318e50fe0
iAIex <32507826+iAIex@users.noreply.github.com>
2017-12-04 10:14:17 +0100

    added Post for file upload on backend

--- Filehost/Filehost/server.js
+++ Filehost/Filehost/server.js
@@ -1,8 +1,9 @@
 'use strict';
-var express = require('express');
-var app = require('express')();
-var http = require('http').Server(app);
-var mysql = require('mysql');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = require('express')();
+const http = require('http').Server(app);
+const mysql = require('mysql');
 
 //Listening on Port 1337
 http.listen(1337, function () {
@@ -16,10 +17,36 @@ app.get('/', function (req, res) {
     console.log("Root Requested by " + req.ip);
     res.sendFile(__dirname + '/static/dummy.html')
 })
+
+app.get('/ajax', function (req, res) {
+    console.log("AJAX Requested by " + req.ip);
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hallo");
+})
+
 app.get('/getFiles:id-:token', function (req, res) {
     console.log("AJAX Requested by " + req.ip);
-    console.log("   ID: "+req.params.id);
+    console.log("   ID: " + req.params.id);
     console.log("Token: "+req.params.token);
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("HANS");
 })
+
+app.use(fileUpload());
+
+app.post('/upload', function (req, res) {
+
+    console.log(req.files);
+
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+
+    var myFile = req.files.myFile; //Name of input field
+
+    myFile.mv("userfiles/ "+myFile.name, function (err) { //move (mv()) file to userfiles
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+});
