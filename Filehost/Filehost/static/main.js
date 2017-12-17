4cfca5455b1a8db55a6b087d0bbcb174ce15395a
paddy24041998 <32507841+paddy24041998@users.noreply.github.com>
2017-12-17 22:18:03 +0100

    Dateiauflistung mit Ajax Request Seite 7-->index.html index.html-->index2.html

--- Filehost/Filehost/static/main.js
+++ Filehost/Filehost/static/main.js
@@ -1,29 +1,31 @@
-new Vue({
+var files = new Vue({
   el: "#files",
   data: {
-    sortKey: 'Datum',
-    reverse: false,
-    search:'',
-    columns: ['id', 'name', 'Datum', 'Teilen mit'],
-    newFile:{},
-    tableData[
-      {id: 1, name: Mix1, Datum: 1.12.2017, Teilen mit: Niemanden},
-      {id: 2, name: Mix2, Datum: 2.12.2017, Teilen mit: Schredi,
-      {id: 3, name: Mix3, Datum: 3.12.2017, Teilen mit: Alex},
-      {id: 4, name: Mix4, Datum: 4.12.2017, Teilen mit: Niemanden},
-      {id: 5, name: Mix5, Datum: 11.12.2017, Teilen mit: Niemanden},
-    ]
-  },
-  methods:{
-    sortBy:
-    function(sortKey){
-      this.reverse = (this.sortKey == sortKey) ?! this.reverse : false;
+    myFiles:[],
 
-      this.sortKey = sortKey;
-    },
-    addFile: function(){
-      this.file.push(this.newFile);
-      this.newFile={};
-    }
+    sharedFiles:[]
   }
 });
+
+function myFiles(json){
+    files.myFiles = [];
+    for(i=0; i<json.length; i++){
+      var temp = {};
+      temp.name = json[i].filename;
+      temp.datum = '21.01.2009';
+      temp.teilenMit = 'Alex';
+      files.myFiles.push(temp);
+    }
+}
+
+function ajaxTest()
+{
+   xmlhttp = new XMLHttpRequest();
+   xmlhttp.open("GET",window.location.href+"getUserFiles2", true);
+   xmlhttp.onreadystatechange=function(){
+         if (xmlhttp.readyState==4 && xmlhttp.status==200){
+            myFiles(JSON.parse(xmlhttp.responseText));
+         }
+   }
+   xmlhttp.send();
+}
